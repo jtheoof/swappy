320dae02d0c6dca3fa2fd7ca934a85483ac2dd35
pathetic-lynx <unknown>
2020-02-18 15:17:20 +0200

    fix(application): fix file loop and use of GTK object after lifecycle
    Swappy tried to access Gtk data after the end of the mainloop and the
    file save routine had infinite recursion

--- src/pixbuf.c
+++ src/pixbuf.c
@@ -13,18 +13,8 @@ GdkPixbuf *pixbuf_get_from_state(struct swappy_state *state) {
   return pixbuf;
 }
 
-void pixbuf_save_state_to_folder(GdkPixbuf *pixbuf, char *folder) {
+static void write_file(GdkPixbuf *pixbuf, char *path) {
   GError *error = NULL;
-
-  time_t current_time;
-  char *c_time_string;
-
-  time(&current_time);
-
-  c_time_string = ctime(&current_time);
-  c_time_string[strlen(c_time_string) - 1] = '\0';
-  char path[MAX_PATH];
-  snprintf(path, MAX_PATH, "%s/%s %s.png", folder, "Swappshot", c_time_string);
   gdk_pixbuf_savev(pixbuf, path, "png", NULL, NULL, &error);
 
   if (error != NULL) {
@@ -38,6 +28,19 @@ void pixbuf_save_state_to_folder(GdkPixbuf *pixbuf, char *folder) {
   snprintf(message, len, "%s%s", msg, path);
   notification_send("Swappy", message);
   g_free(message);
+}
+
+void pixbuf_save_state_to_folder(GdkPixbuf *pixbuf, char *folder) {
+  time_t current_time;
+  char *c_time_string;
+
+  time(&current_time);
+
+  c_time_string = ctime(&current_time);
+  c_time_string[strlen(c_time_string) - 1] = '\0';
+  char path[MAX_PATH];
+  snprintf(path, MAX_PATH, "%s/%s %s.png", folder, "Swappshot", c_time_string);
+  write_file(pixbuf, path);
 }
 
 void pixbuf_save_to_stdout(GdkPixbuf *pixbuf) {
@@ -61,6 +64,6 @@ void pixbuf_save_to_file(GdkPixbuf *pixbuf, char *file) {
   if (g_strcmp0(file, "-") == 0) {
     pixbuf_save_to_stdout(pixbuf);
   } else {
-    pixbuf_save_to_file(pixbuf, file);
+    write_file(pixbuf, file);
   }
 }
