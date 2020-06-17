204a93eb0f696bc7be8335d46212c6024e3b2c51
Jeremy Attali <jeremy.attali@gmail.com>
2020-06-16 23:02:58 -0400

    refactor!(wayland): remove wayland code
    BREAKING CHANGE: We do no support the `-g` option anymore.
    
    This tool simply makes more sense as the output of `grim` rather than
    trying to be `grim`.
    
    RIP my ugly wayland code, long live maintainable code.
    
    Next stop, rust?

--- include/buffer.h
+++ include/buffer.h
@@ -1,17 +1,7 @@
 #pragma once
 
-#include <stdio.h>
-#include <wayland-client.h>
-
 #include "swappy.h"
 
-void buffer_wayland_destroy(struct swappy_buffer *buffer);
-
-bool buffer_init_from_screencopy(struct swappy_state *state);
 bool buffer_init_from_file(struct swappy_state *state);
-
-bool buffer_parse_geometry(struct swappy_state *state);
-
 void buffer_resize_patterns(struct swappy_state *state);
-
 void buffer_free_all(struct swappy_state *state);
