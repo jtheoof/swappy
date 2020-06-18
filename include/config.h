307f57956f105d22de2d8242313517b6a79ed4e2
Jeremy Attali <jeremy.attali@gmail.com>
2020-06-17 22:54:47 -0400

    feat(config): add show_panel config
    Allows to toggle paint panel on/off upon startup.
    
    Closes #12

--- include/config.h
+++ include/config.h
@@ -3,6 +3,7 @@
 #define CONFIG_LINE_SIZE_DEFAULT 5
 #define CONFIG_TEXT_FONT_DEFAULT "sans-serif"
 #define CONFIG_TEXT_SIZE_DEFAULT 20
+#define CONFIG_SHOW_PANEL_DEFAULT false
 
 void config_load(struct swappy_state *state);
 void config_free(struct swappy_state *state);
