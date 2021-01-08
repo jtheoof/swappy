eb53e5c2b28717f509dd58eab6da85897c0d6d9d
snakedye <49378990+snakedye@users.noreply.github.com>
2021-01-08 16:18:14 -0500

    fix(notification): notification shows the image icon

--- src/notification.c
+++ src/notification.c
@@ -8,7 +8,7 @@ void notification_send(char *title, char *message) {
 #ifdef HAVE_LIBNOTIFY
   notify_init("Hello world!");
   NotifyNotification *notification =
-      notify_notification_new(title, message, "dialog-information");
+      notify_notification_new(title, message, "image");
   notify_notification_show(notification, NULL);
   g_object_unref(G_OBJECT(notification));
   notify_uninit();
