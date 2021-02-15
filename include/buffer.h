5bcffdbb01cc6e56f9c0f37de899b46efe68ed4a
Jeremy Attali <contact@jtheoof.me>
2021-02-14 21:06:41 -0500

    fix(ui): compute window sizes and buffers properly
    Compute maximum width and height of area container based on monitor size
    and fix image ratio based on appropriate values.
    
    Note that this will lower the resolution of the final image.
    
    Closes #56

--- include/buffer.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "swappy.h"
-
-bool buffer_init_from_file(struct swappy_state *state);
-void buffer_resize_patterns(struct swappy_state *state);
-void buffer_free_all(struct swappy_state *state);
--- include/pixbuf.h
+++ include/pixbuf.h
@@ -2,8 +2,11 @@
 
 #include "swappy.h"
 
+GdkPixbuf *pixbuf_init_from_file(struct swappy_state *state);
 GdkPixbuf *pixbuf_get_from_state(struct swappy_state *state);
 void pixbuf_save_state_to_folder(GdkPixbuf *pixbuf, char *folder,
                                  char *filename_format);
 void pixbuf_save_to_file(GdkPixbuf *pixbuf, char *file);
 void pixbuf_save_to_stdout(GdkPixbuf *pixbuf);
+void pixbuf_scale_surface_from_widget(struct swappy_state *state,
+                                      GtkWidget *widget);
