30c889f4db130fb22abd3aed0b23cc1bf15d8b51
Jeremy Attali <contact@jtheoof.me>
2022-12-12 16:49:35 -0500

    try iced

--- src/config.rs
+++ src/config.rs
@@ -6,6 +6,8 @@ pub struct Config {
     line_size: u32,
     text_size: u32,
     text_font: String,
+    early_exit: bool,
+    save_filename_format: String,
 }
 
 impl Config {
@@ -14,8 +16,10 @@ impl Config {
             save_dir: String::from("test"),
             show_panel: true,
             line_size: 5,
-            text_size: 10,
-            text_font: String::from("monospace"),
+            text_size: 20,
+            text_font: String::from("sans-serif"),
+            early_exit: false,
+            save_filename_format: String::from("swappy-%Y%m%d_%H%M%S.png"),
         }
     }
 }
