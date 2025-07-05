f0849d8dd2fae4fa62bbe78fc4aefc793aa20881
Oto Šťáva <oto.stava@fghj.cz>
2025-07-05 13:17:25 +0200

    feat(ui): crop: recreate rect when cursor outside

--- src/application.c
+++ src/application.c
@@ -536,6 +536,15 @@ gboolean draw_area_configure_handler(GtkWidget *widget,
   return TRUE;
 }
 
+static
+bool should_crop_recreate(const struct swappy_state *state,
+                         double x, double y,
+                         bool is_control_pressed) {
+  return is_control_pressed || !state->crop_ever_changed ||
+         x < state->crop.left_x || x > state->crop.right_x ||
+         y < state->crop.top_y || y > state->crop.bottom_y;
+}
+
 void draw_area_button_press_handler(GtkWidget *widget, GdkEventButton *event,
                                     struct swappy_state *state) {
   gdouble x, y;
@@ -556,11 +565,14 @@ void draw_area_button_press_handler(GtkWidget *widget, GdkEventButton *event,
         render_state(state);
         update_ui_undo_redo(state);
         break;
-      case SWAPPY_PAINT_MODE_CROP:
-        paint_start_crop(&state->crop, x, y,
-                         is_control_pressed || !state->crop_ever_changed);
+      case SWAPPY_PAINT_MODE_CROP: {
+        gboolean recreate = should_crop_recreate(state, x, y,
+                                                 is_control_pressed);
+        paint_start_crop(&state->crop, x, y, recreate);
         state->crop_ever_changed = true;
         render_state(state);
+        break;
+      }
       default:
         return;
     }
@@ -578,8 +590,8 @@ void set_cursor(GdkWindow *window, GdkCursorType cursor_type) {
 static
 GdkCursorType get_crop_cursor_type(struct swappy_state *state,
                                    gdouble x, gdouble y,
-                                   gboolean is_control_pressed) {
-  if (is_control_pressed || !state->crop_ever_changed)
+                                   gboolean recreate) {
+  if (recreate)
     return GDK_CROSSHAIR;
 
   enum swappy_resize resize_x, resize_y;
@@ -651,12 +663,15 @@ void draw_area_motion_notify_handler(GtkWidget *widget, GdkEventMotion *event,
         render_state(state);
       }
       break;
-    case SWAPPY_PAINT_MODE_CROP:
-      cursor_type = get_crop_cursor_type(state, x, y, is_control_pressed);
+    case SWAPPY_PAINT_MODE_CROP: {
+      gboolean recreate = should_crop_recreate(state, x, y, is_control_pressed);
+      cursor_type = get_crop_cursor_type(state, x, y, recreate);
       if (is_button1_pressed) {
         paint_update_crop(&state->crop, x, y);
         render_state(state);
       }
+      break;
+    }
     default:
       break;
   }
